17b2c1dc405f6eed1526a03e4eaaf8bbdb978fa6
Zheer Kejlberg Al-Mashhadi <102064238+zheer-kejlberg@users.noreply.github.com>
2026-02-02 15:43:34 +0100

    updated notes, especially pipeline example

--- Misc/to asciicast.R
+++ Misc/to asciicast.R
@@ -1,11 +1,11 @@
-asciicast::write_gif(asciicast::record("r-coding-cafe/rcc_gif.R"), 
+asciicast::write_gif(asciicast::record("Misc/rcc_gif.R"), 
                      "img/rcc_gif.gif", 
-                     speed=2,
+                     speed=2.5,
                      rows = 13,
                      cols = 55,
                      theme = "solarized-light")
 
-asciicast::write_gif(asciicast::record("r-coding-cafe/rcc_gif2.R"), 
+asciicast::write_gif(asciicast::record("Misc/rcc_gif2.R"), 
                      "img/rcc_gif2.gif", 
                      speed=2.5,
                      rows = 13,
@@ -13,8 +13,8 @@ asciicast::write_gif(asciicast::record("r-coding-cafe/rcc_gif2.R"),
                      theme = "solarized-light")
 
 
-asciicast::write_gif(asciicast::record("r-coding-cafe/rcc_gif3.R"), 
-                     "img/rcc_gif3.gif", 
+asciicast::write_gif(asciicast::record("Misc/rcc_gif3.R"), 
+                     "img/rcc_gif5.gif", 
                      speed=2.5,
                      rows = 3,
                      cols = 40,
