7b9d62c1afbd8ad0801e76615cccba48b199abee
Zheer Kejlberg Al-Mashhadi <102064238+zheer-kejlberg@users.noreply.github.com>
2026-02-02 22:53:54 +0100

    updated github actions

--- scripts/render-site.R
+++ scripts/render-site.R
@@ -1,9 +1,3 @@
 # scripts/render_site.R
 
-quarto::quarto_render("index.qmd", output_format = "html")
-quarto::quarto_render("practical-info.qmd", output_format = "html")
-quarto::quarto_render("notes/extra/1-example-pipeline.qmd", output_format = "html")
-quarto::quarto_render("notes/extra/index.qmd", output_format = "html")
-quarto::quarto_render("notes/lesson-notes/index.qmd", output_format = "html")
-quarto::quarto_render("notes/lesson-notes/3-data-types.qmd", output_format = "html")
-quarto::quarto_render("notes/lesson-notes/1-minimal-reproducible-example.qmd", output_format = "html")
+quarto::quarto_render(".", output_format = "html")
