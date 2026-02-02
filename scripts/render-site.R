64af066d506b8f9074112e2a9aa46d78c0aa7188
Zheer Kejlberg Al-Mashhadi <102064238+zheer-kejlberg@users.noreply.github.com>
2026-02-02 22:23:13 +0100

    tried adding github actions

--- scripts/render-site.R
+++ scripts/render-site.R
@@ -1,10 +1,9 @@
 # scripts/render_site.R
 
-
-cat("Working directory:", getwd(), "\n")
-
-# Render multiple formats of the same document
 quarto::quarto_render("index.qmd", output_format = "html")
 quarto::quarto_render("practical-info.qmd", output_format = "html")
-quarto::quarto_render("notes.qmd", output_format = "html")
-quarto::quarto_render("notes/minimal-reproducible-example.qmd", output_format = "html")
+quarto::quarto_render("notes/extra/1-example-pipeline.qmd", output_format = "html")
+quarto::quarto_render("notes/extra/index.qmd", output_format = "html")
+quarto::quarto_render("notes/lesson-notes/index.qmd", output_format = "html")
+quarto::quarto_render("notes/lesson-notes/3-data-types.qmd", output_format = "html")
+quarto::quarto_render("notes/lesson-notes/1-minimal-reproducible-example.qmd", output_format = "html")
